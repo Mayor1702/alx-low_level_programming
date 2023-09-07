7148096c9883d9ee63cdb30c1119756dba739211
Mayor1702 <lawalmayowa1702@gmail.com>
2023-09-07 16:26:52 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - this is a function that tells the compiler what to interprete
+ *
+ * Return: always 0 in order to end a program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
