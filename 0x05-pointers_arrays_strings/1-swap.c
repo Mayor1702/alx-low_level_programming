d80b4bedbf239f7ce5e1d19abf87cd5ccaebbb23
Mayor1702 <lawalmayowa1702@gmail.com>
2023-09-18 14:18:55 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: value1
+ * @b: value2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+}
